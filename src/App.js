b35a3cdbea042b9821dc4930b24737fb97ad0f52
Boka88 <boka8boka@gmail.com>
2022-01-21 02:05:59 +0100

    add box wrapper in navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Button} from '@mui/material'
 import Navbar from './components/Navbar/Navbar'
 
+
 // https://www.youtube.com/watch?v=BTgBcsSZxFU&list=PLakAmVjYWIY6g-Bx2vVnHFuaa8oaPorSI&index=2&ab_channel=iNovotek 4 Deo 1:00
 
 function App() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,61 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material'
+import { 
+    AppBar, 
+    Toolbar, 
+    IconButton, 
+    Typography, 
+    Button,
+    Badge
+} from '@mui/material'
+
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+import { Box } from '@mui/system';
 
 const Navbar = () => {
-    
-    return(
-        <AppBar elevation={0} color='secondary'>
-            <Toolbar>
-                <IconButton>
-                    <MenuOutlinedIcon />
-                </IconButton>
 
-                <Typography variant="h2">Home</Typography>
+    return (
+        <AppBar elevation={0} sx={{ backgroundColor: 'white' }}>
+            <Toolbar>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        width: '100%'
+                    }}
+                    component='div'
+                >
+                    {/* Logo */}
+                    <Box>
+                        <IconButton>
+                            <MenuOutlinedIcon />
+                        </IconButton>
+                    </Box>
+
+                    {/* Link */}
+                    <Box sx={{ display: 'flex' }}>
+                        <Typography sx={{ marginRight: '20px', cursor: 'pointer', color: '#616161' }}>Home</Typography>
+                        <Typography sx={{ marginRight: '20px', cursor: 'pointer', color: '#616161' }}>Brand</Typography>
+                        <Typography sx={{ marginRight: '20px', cursor: 'pointer', color: '#616161' }}>Men</Typography>
+                        <Typography sx={{ marginRight: '20px', cursor: 'pointer', color: '#616161' }}>Women</Typography>
+                        <Typography sx={{ marginRight: '20px', cursor: 'pointer', color: '#616161' }}>FAQ</Typography>
+                    </Box>
+
+                    {/* Button links */}
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Button sx={{backgroundColor: '#ff4081'}} disableElevation variant='contained'>
+                            Account
+                        </Button>
+                        <IconButton>
+                            <Badge badgeContent={4} color="primary">
+                                <ShoppingBasketIcon color='action' />
+                            </Badge>
+                        </IconButton>
+                    </Box>
+
+                </Box>
             </Toolbar>
         </AppBar>
     )
