c7b6d278289368eacf3f8525ba5978a061944185
Boka88 <boka8boka@gmail.com>
2022-01-21 19:07:56 +0100

    add product container component featured product section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header';
+import GridMUI from './playground/GridMUI';
+import ProductContainer from './components/Products/ProductContainer';
 
 // https://www.youtube.com/watch?v=BTgBcsSZxFU&list=PLakAmVjYWIY6g-Bx2vVnHFuaa8oaPorSI&index=2&ab_channel=iNovotek 6 Deo Pocetak
 
@@ -9,6 +11,7 @@ function App() {
     <>
       <Navbar />
       <Header />
+      <ProductContainer />
     </>
   );
 }
